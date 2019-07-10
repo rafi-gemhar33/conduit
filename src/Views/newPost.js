5c74a56a08884b1e32afef4f97f7edc518b0948d
nnnkit <iankit17@gmail.com>
2019-07-10 16:28:38 +0530

    refactor login and signup page

--- src/Views/newPost.js
+++ src/Views/newPost.js
@@ -1,40 +1,49 @@
-import React from 'react';
+import React from "react";
 
 export default function NewArticle() {
   return (
     <div className="column is-10 is-offset-1">
       <div className="field">
-          <div className="control">
-          <input className="input is-medium" type="text" placeholder="Article Title" />
-          </div>
-      </div>
-    
-      <div className="field">
-
         <div className="control">
-          <input className="input is-medium" type="text" placeholder="What's the article about?" />
+          <input
+            className="input is-medium"
+            type="text"
+            placeholder="Article Title"
+          />
         </div>
       </div>
-      
       <div className="field">
         <div className="control">
-          <textarea className="textarea is-medium" placeholder="Write your article (in markdown)" rows="5"></textarea>
+          <input
+            className="input is-medium"
+            type="text"
+            placeholder="What's the article about?"
+          />
         </div>
       </div>
-
       <div className="field">
         <div className="control">
-          <input className="input is-medium" type="text" placeholder="Enter Tags" />
+          <textarea
+            className="textarea is-medium"
+            placeholder="Write your article (in markdown)"
+            rows="5"
+          />
         </div>
       </div>
-
-      
+      <div className="field">
+        <div className="control">
+          <input
+            className="input is-medium"
+            type="text"
+            placeholder="Enter Tags"
+          />
+        </div>
+      </div>
       <div className="field is-grouped">
         <div className="control">
           <button className="button is-success is-medium">Submit</button>
         </div>
-
       </div>
     </div>
-   )
+  );
 }
--- todo.md
+++ todo.md
@@ -1,3 +1,7 @@
 ## Articles
 * [ ] [Presentational and Container Components](https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0)
 * [ ] [NavLink](https://github.com/ReactTraining/react-router/blob/master/packages/react-router-dom/docs/api/NavLink.md)
+
+## Things to do
+
+1. Refactor you signup page.
