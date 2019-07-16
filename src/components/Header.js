b7ad0ca957f1fa6d5ca30e6c3d05a29bb0eaf5d6
Gemhar33 <gemharedu@gmail.com>
2019-07-16 09:59:42 +0530

    adding implementation to navbar so it becomes responsive in touchscreen view

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,79 +11,91 @@ class Header extends React.Component {
 
     return (
       <header className="base column is-8 is-offset-2">
-        <nav className="navbar" role="navigation" aria-label="main navigation">
+        <nav className="navbar " role="navigation" aria-label="main navigation">
           <div className="navbar-brand">
-            <h1 className="title is-3 isPrimary">
+            <h1 className="title is-3 logo isPrimary">
               <Link className="navbar-item green-text" to="/">
                 conduit
               </Link>
             </h1>
+
+            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
           </div>
+
           <div id="navbarBasicExample" className="navbar-menu">
             <div className="navbar-end">
-              <NavLink
-                exact
-                className="navbar-item"
-                to="/"
-                activeClassName="active__nav"
-              >
-                Home
-              </NavLink>
-              {auth.isLogged() ? (
-                <>
-                  <NavLink
-                    exact
-                    className="navbar-item"
-                    to={{
-                      pathname: '/newArticle',
-                      state: { }
-                    }}
-                    activeClassName="active__nav"
-                  >
-                    New Post
-                  </NavLink>
-                  <NavLink
-                    exact
-                    className="navbar-item"
-                    to="/editUser"
-                    activeClassName="active__nav"
-                  >
-                    Settings
-                  </NavLink>
-                  <NavLink
-                    exact
-                    className="navbar-item"
-                    to={{
-                      pathname: "/profile",
-                      state: {
-                        username: user.username
-                      }
-                    }}
-                    activeClassName="active__nav"
-                  >
-                    {user.username}
-                  </NavLink>
-                </>
-              ) : (
-                <>
-                  <NavLink
-                    exact
-                    className="navbar-item"
-                    to="/login"
-                    activeClassName="active__nav"
-                  >
-                    Sign In
-                  </NavLink>
-                  <NavLink
-                    exact
-                    className="navbar-item"
-                    to="/register"
-                    activeClassName="active__nav"
-                  >
-                    Sign Up
-                  </NavLink>
-                </>
-              )}
+              <div class="navbar-item ">
+                
+
+                <NavLink
+                  exact
+                  className="navbar-item"
+                  to="/"
+                  activeClassName="active__nav"
+                >
+                  Home
+                </NavLink>
+                {auth.isLogged() ? (
+                  <>
+                    <NavLink
+                      exact
+                      className="navbar-item"
+                      to={{
+                        pathname: '/newArticle',
+                        state: { }
+                      }}
+                      activeClassName="active__nav"
+                    >
+                      New Post
+                    </NavLink>
+                    <NavLink
+                      exact
+                      className="navbar-item"
+                      to="/editUser"
+                      activeClassName="active__nav"
+                    >
+                      Settings
+                    </NavLink>
+                    <NavLink
+                      exact
+                      className="navbar-item"
+                      to={{
+                        pathname: "/profile",
+                        state: {
+                          username: user.username
+                        }
+                      }}
+                      activeClassName="active__nav"
+                    >
+                      {user.username}
+                    </NavLink>
+                  </>
+                ) : (
+                  <>
+                    <NavLink
+                      exact
+                      className="navbar-item"
+                      to="/login"
+                      activeClassName="active__nav"
+                    >
+                      Sign In
+                    </NavLink>
+                    <NavLink
+                      exact
+                      className="navbar-item"
+                      to="/register"
+                      activeClassName="active__nav"
+                    >
+                      Sign Up
+                    </NavLink>
+                  </>
+                )}
+              </div>
+
             </div>
           </div>
         </nav>
